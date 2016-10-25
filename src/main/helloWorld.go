fada82878a6e56d0b1cfdb091d76a421b45793e8
zengxiangxin <aaazengxx@163.com>
2016-10-25 23:44:40 +0800

    edit

--- src/main/helloWorld.go
+++ src/main/helloWorld.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+var count int
+var chans []chan int
 
 func main() {
 	fmt.Println("Hello World !")
+	
 
+	var size=10
+	chans=make([]chan int,10)
+
+	for i:=0;i<size;i++{
+		go test(i)
+	}
+
+	for j:=0;j<size;j++{
+
+		<-chans[j]
+	}
+
+}
+
+func test(i int){
+	count ++;
+	fmt.Println(strconv.Itoa(count))
+	chans[i]<-1
 }
